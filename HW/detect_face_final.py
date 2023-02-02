38220335fa5f5a1c45a06c68d0aaf53fe248631c
이준호 <dlwnsgh3619@naver.com>
2023-02-02 17:48:51 +0900

    Update HW/detect_face_final.py

--- HW/detect_face_final.py
+++ HW/detect_face_final.py
@@ -8,25 +8,25 @@ mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 
 @asyncio.coroutine
-def transport_with_backend()
-    websocket = yield from websockets.connect('wsi8c203.p.ssafy.io8002')
+def transport_with_backend():
+    websocket = yield from websockets.connect('ws://i8c203.p.ssafy.io:8002')
 
-    # For webcam input
+    # For webcam input:
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-    camera_id=devvideo0
+    camera_id="/dev/video0"
 
     cap = cv2.VideoCapture(camera_id, cv2.CAP_V4L2)
-    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('YUYV'))
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'YUYV'))
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     with mp_face_mesh.FaceMesh(
         min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as face_mesh
+        min_tracking_confidence=0.5) as face_mesh:
     
-        while cap.isOpened()
+        while cap.isOpened():
             success, image = cap.read()
-            if not success
-                print(033[91m + Ignoring empty camera frame. + 033[0m)
+            if not success:
+                print("\033[91m" + "Ignoring empty camera frame." + "\033[0m")
                 # If loading a video, use 'break' instead of 'continue'.
                 break
 
@@ -41,28 +41,30 @@ def transport_with_backend()
             # Draw the face mesh annotations on the image.
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            face_lists = [nose, Top, upperLib, lowerLib, leftEye, Chin, Cheek2, Cheek1, rightEye, Cheek3]
-
-            
-            if results.multi_face_landmarks
-                for face_landmark in results.multi_face_landmarks
-                    landmarks_tong = {}
-                    iterator = 0
-                    for id, lm in enumerate(face_landmark.landmark)
-                        if id in (10, 27, 257, 1, 11, 17, 207, 204, 434, 152)
-                            ih, iw, ic = image.shape
-                            x, y = int(lm.xiw), int(lm.yih)
-                            landmarks_tong[face_lists[iterator]] = list((x,y))
-                            iterator += 1
-                            cv2.drawMarker(image, (x, y), (255, 0, 0), markerType=cv2.MARKER_STAR, markerSize = 10)
-                        #print(landmarks_tong)
+   
+            if results.multi_face_landmarks:
+                for face_landmark in results.multi_face_landmarks:
+                    ih, iw, ic = image.shape
+                    landmarks = list(face_landmark.landmark)
+                    landmarks_tong = {
+                        "nose": str(int(landmarks[1].x * iw)) + ',' + str(int(landmarks[1].y * ih)),
+                        "Top": str(int(landmarks[10].x * iw)) + ',' + str(int(landmarks[10].y * ih)),
+                        "upperLib": str(int(landmarks[11].x * iw)) + ',' + str(int(landmarks[11].y * ih)),
+                        "lowerLib": str(int(landmarks[17].x * iw)) + ',' + str(int(landmarks[17].y * ih)),
+                        "leftEye": str(int(landmarks[27].x * iw)) + ',' + str(int(landmarks[27].y * ih)),
+                        "Chin": str(int(landmarks[152].x * iw)) + ',' + str(int(landmarks[152].y * ih)),
+                        "Cheek2": str(int(landmarks[204].x * iw)) + ',' + str(int(landmarks[204].y * ih)),
+                        "Cheek1": str(int(landmarks[207].x * iw)) + ',' + str(int(landmarks[207].y * ih)),
+                        "rightEye": str(int(landmarks[257].x * iw)) + ',' + str(int(landmarks[257].y * ih)),
+                        "Cheek3": str(int(landmarks[434].x * iw)) + ',' + str(int(landmarks[434].y * ih))
+                    }
                     Tong = json.dumps(landmarks_tong)
                     yield from websocket.send(Tong)
             cv2.imshow('MediaPipe FaceMesh', image)
-            if cv2.waitKey(1) & 0xFF == 27
+            if cv2.waitKey(1) & 0xFF == 27:
                 yield from websocket.close()
                 break
-    cap.release()
-    cv2.destroyAllWindows()          
+        cap.release()
+        cv2.destroyAllWindows()
               
 asyncio.get_event_loop().run_until_complete(transport_with_backend())
