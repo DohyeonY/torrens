a51c354c78b00288b7f6948bd16c67ab4eee6d7c
여도현 <ydh51216@gmail.com>
2023-02-06 08:52:19 +0900

    Feat_waitpages

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -7,19 +7,26 @@ import Run from './component/Run'
 import Hello from './component/Hello'
 import HelloGif from './component/HelloGif'
 import WebSoket from './component/WebSoket'
+import CatchBf from './component/WaitScreen/CatchBF'
+import Sleep from './component/WaitScreen/Sleep'
+import EatLunch from './component/WaitScreen/EatLunch'
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"
+    >
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main/>}/>
+          <Route path="/main" element={<Main/>}/>
           <Route path="/next" element={<Next/>}/>
           <Route path="/run" element={<Run/>}/>
           <Route path="/hello" element={<Hello/>}/>
           <Route path="/hellogif" element={<HelloGif/>}/>
           <Route path="/websoket" element={<WebSoket/>}/>
+          <Route path="/" element={<CatchBf/>}/>
+          <Route path="/sleep" element={<Sleep/>}/>
+          <Route path="/eatlunch" element={<EatLunch/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ FrontEnd/src/component/WaitScreen/EatLunch.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import "./CatchBF.css"
+
+export default function CatchBF() {
+    const navigate = useNavigate()
+    const [timer, setTimer] = useState("00:00:00")
+    const [hour, setHour] = useState("")
+    const [exist, setExist] = useState("")
+
+    const currentTimer = () => {
+        const date = new Date()
+        const hours = String(date.getHours()).padStart(2, "0")
+        const minutes = String(date.getMinutes()).padStart(2, "0")
+        const seconds = String(date.getSeconds()).padStart(2, "0")
+        setTimer(`${hours}시 ${minutes}분 ${seconds}초`)
+        setHour(`${hours}`)
+
+    }
+    const readyChange = () => {
+        let ws = new WebSocket("ws://i8c203.p.ssafy.io:8002")
+        ws.onmessage = (event) => {
+            setExist(event.data.Top)
+        }
+    }
+
+    const startTimer = () => {
+        setInterval(currentTimer, 1000)
+        // console.log(hour)
+        if (hour === "14") {
+            navigate("/sleep")
+        } else if (hour !== "12") {
+            navigate("/")
+        }
+        if (exist !== "") {
+            setExist("")
+            navigate("/main")
+        }
+    }
+    readyChange()
+    startTimer()
+    return (
+        <div>
+            <div style={{
+                width: "1536px",
+                height: "864px",
+                // backgroundImage: "url(img/wait/EatLunch.gif)" 
+            }}>
+                <h1 style={{
+                    textAlign: "left",
+                    padding: "50px",
+                    margin: "0px",
+                    color: "white"
+                }}>{timer}</h1>
+                <img style={{
+                    paddingRight: "800px",
+                }}
+                    src="img/wait/EatLunch.gif" alt="123" />
+            </div>
+        </div>
+    )
+}
