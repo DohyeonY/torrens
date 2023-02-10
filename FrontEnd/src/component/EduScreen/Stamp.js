1a8655a031ae45ddaaad6bb9c22378c59ef7afc6
여도현 <ydh51216@gmail.com>
2023-02-10 17:30:07 +0900

    Feat_add_stamp_component

--- FrontEnd/src/component/EduScreen/Stamp.js
+++ FrontEnd/src/component/EduScreen/Stamp.js
@@ -1,21 +1,47 @@
 import React from 'react';
 
-// import { useEffect } from "react";
-// import { useState } from "react";
-// import { io } from "socket.io-client"
-// import { useInView } from "framer-motion";
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./WebSoket.css"
-// import '~video-react/dist/video-react.css';
-// import { Player } from 'video-react';
 import UseInterval from "../UseInterval"
+import { useNavigate } from "react-router-dom";
+import WebSoket2 from './Websoket2';
 
-export default function EduScreen () {
+function Stamp() {
+    const navigate = useNavigate()
+    const readyChange = () => {
+        let ws = new WebSocket("ws://i8c203.p.ssafy.io:8003/command")
+        ws.onmessage = (event) => {
+          // setpressButton(event.data)
+          if ( event.data === "OK" ) {
+            navigate("/")
+          }
+          console.log(event.data)
+        }
+    }
+      readyChange()
 
     return (
         <>        
-            <img src="img/edu/stamp.gif" alt="dd"/>
+        <div
+         style={{
+            marginLeft: "0px",
+            marginTop: "150px",
+            float: "left",
+            height: "1080px",
+            width: "50%",
+         }}
+        >
+            <img style={{
+                // floate: "left",
+                height: "80%",
+                width: "150%",
+            }} 
+            src="img/edu/stamp.gif" alt="dd"/>
+        </div>
+        <WebSoket2/>
         </>
 
         )
     }
+
+export default Stamp;
